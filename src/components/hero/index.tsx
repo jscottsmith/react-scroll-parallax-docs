4f87672b0b17e6b117a6d2c7eb1e880f34b62827
J Scott Smith <jscsmith@gmail.com>
2021-12-06 23:00:17 -0800

    better hero styles

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -4,12 +4,20 @@ import { Parallax } from "react-scroll-parallax"
 const PARALLAX = ["P", "A", "R", "A", "L", "L", "A", "X"]
 export const Hero = () => {
   return (
-    <article className="bg-purple-300 h-screen flex items-center justify-center">
-      <h1 className="text-4xl flex gap-md">
+    <article className="px-bg bg-blue-500 h-screen flex items-center justify-center overflow-hidden">
+      <h1 className="text-3xl flex items-center gap-md">
         {PARALLAX.map((letter, i) => (
           <Parallax key={i} speed={-i - i * 2}>
-            <span className="flex w-24 h-24 bg-purple-200 rounded-md items-center justify-center">
-              {letter}
+            <span className="inline-block border-solid border-blue-900 border-2 rounded-lg p-1">
+              <span
+                className="flex bg-blue-400 rounded-md items-center justify-center"
+                style={{
+                  width: 50 + i * 6,
+                  height: 50 + i * 6,
+                }}
+              >
+                {letter}
+              </span>
             </span>
           </Parallax>
         ))}
