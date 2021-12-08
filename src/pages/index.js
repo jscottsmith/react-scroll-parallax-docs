8ceb3cccbce2cff1512325230b9932062e92010d
J Scott Smith <jscsmith@gmail.com>
2021-12-07 22:01:31 -0800

    css effects

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,6 +33,25 @@ export default function Home() {
         </div>
       </SourceDemo>
       <ElementProgress />
+      <SourceDemo
+        title="Beyond Parallax: CSS effects"
+        description="Additional CSS effects, like opacity, scale and rotation can be applied based on progress, even with some easing."
+        source={`<Parallax\n  translateX={["-400px", "0px"]}\n  scale={[0.75, 1]}\n  rotate={[-180, 0]}\n  easing="easeInQuad"\n>`}
+      >
+        <div className="flex flex-row items-center justify-evenly w-full">
+          <Parallax
+            translateX={["-400px", "0px"]}
+            scale={[0.75, 1]}
+            rotate={[-180, 0]}
+            easing="easeInQuad"
+            className="rounded-lg bg-gray-600 bg-opacity-50"
+          >
+            <div className="border-2 border-blue-200 border-solid flex items-center justify-center bg-blue-400 h-48 w-48 rounded-lg">
+              <p className="text-center font-bold uppercase">Heyo!</p>
+            </div>
+          </Parallax>
+        </div>
+      </SourceDemo>
       <Header />
     </PageContainer>
   )
