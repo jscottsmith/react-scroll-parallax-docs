8e432609f3c3303bb942687cc6c917149c0e37fd
J Scott Smith <jscsmith@gmail.com>
2021-12-28 11:13:48 -0800

    github repo link

--- src/components/header/navigation/index.tsx
+++ src/components/header/navigation/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { GithubLogo } from "../../github-logo"
 import { NavLink } from "./nav-link"
 
 export const Navigation = () => {
@@ -7,6 +8,14 @@ export const Navigation = () => {
       <div className="flex gap-2">
         <NavLink to="/speed">Speed</NavLink>
         <NavLink to="/easing">Easing</NavLink>
+        <NavLink
+          to="https://github.com/jscottsmith/react-scroll-parallax"
+          aria-label="GitHub Repository"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <GithubLogo />
+        </NavLink>
       </div>
     </nav>
   )
