c5619b1a2a2fedb2940348bd17e926f51f2c2892
J Scott Smith <jscsmith@gmail.com>
2021-12-28 22:30:08 -0800

    nav link spacing

--- src/components/header/navigation/index.tsx
+++ src/components/header/navigation/index.tsx
@@ -5,7 +5,7 @@ import { NavLink } from "./nav-link"
 export const Navigation = () => {
   return (
     <nav className="flex justify-between">
-      <div className="flex gap-2">
+      <div className="flex gap-2 md:gap-5">
         <NavLink to="/speed">Speed</NavLink>
         <NavLink to="/easing">Easing</NavLink>
         <a
